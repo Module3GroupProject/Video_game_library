97c2c45f7712ded853abb446d95fd55b93c0bd3a
Jiyah <jiyahcoleman00@gmail.com>
2025-06-28 12:48:08 -0500

    Update App.js

--- server/App.js
+++ server/App.js
@@ -14,14 +14,11 @@ const client = new MongoClient(mongodb_uri)
 app.use(cors())
 app.use(express.json())
 
-//Get all of the user data
 app.get("/userdata", async (request,response)=> {
     try {
         const database = await client.db(db);
         const coll = await database.collection(collection);
         const data = await coll.find().toArray()
-
-        // console.log(data)
 
         response.status(200).send(data)
     } catch (error) {
@@ -29,67 +26,7 @@ app.get("/userdata", async (request,response)=> {
     }
 })
 
-//Get one user. This can be used to grab their info once they login.
-app.get("/userdata/:username", async (request,response, next)=>{
-    try {
-        const database = await client.db(db);
-        const coll = await database.collection(collection);
-        const data = await coll.find().toArray()
 
-        let userName = request.params.username
-
-        const usernameData = data.filter((user) =>{
-            return user.username === userName
-        })
-
-        // Conditionally send a response based on whether that data is found or not
-        if (usernameData.length > 0){
-        let json_response = JSON.stringify(usernameData)
-        response.status(200).send(json_response)
-        }else{
-        response.status(404).send("Resource not found")
-        };
-
-    } catch (error) {
-        console.error("Something went wrong: ", error)
-        next(error)
-    }
-    
-})
-
-// Create a new user
-app.post("/userdata", async (request, response)=>{
-    try {let newUser = {...request.body};
-    const database = await client.db(db);
-    const coll = await database.collection(collection);
-    await coll.insertOne(newUser)
-
-  
-    let data = [newUser]
-    response.status(201).send(JSON.stringify([...data]))
-    } catch (error) {
-        console.error("Something went wrong: ", error)
-    }
-})
-
-//Update game data when the user adds the games that they like.
-//This code uses the username to identify what it wants to change.
-app.put("/userdata", async (request, response)=>{
-    try {const database = await client.db(db);
-    const coll = await database.collection(collection);
-    await coll.updateOne({username: request.body.username},
-        {
-            $set:{
-                game_data: request.body.game_data
-            }
-        }
-    )
-    const data = await coll.find().toArray()
-    response.status(200).send(data)
-    } catch (error) {
-        console.error("Something went wrong: ", error)
-    }
-})
 
 // Start the server
 app.listen(PORT, () => {
